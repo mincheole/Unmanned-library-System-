d1d8172aeaede60f4799ee945f3c06cd91f42969
Mincheol Kim <106231571+mincheole@users.noreply.github.com>
2023-04-05 15:24:06 +0900

    DTO 객체 일부 구현
    book, rfid_card 객체 생성

--- BOOK/src/DTO/book.java
+++ BOOK/src/DTO/book.java
@@ -1,5 +1,67 @@
 package DTO;
 
+import java.sql.Date;
+
 public class book {
+	int isbn;
+	String title;
+	String author;
+	String publisher;
+	Date publication_year;
+	String category;
+
+	public book(int p_isbn, String p_title, String p_author,
+				String p_publisher,	Date p_publication_year, String p_category) {
+		this.isbn = p_isbn;
+		this.title = p_title;
+		this.author = p_author;
+		this.publisher = p_publisher;
+		this.publication_year = p_publication_year;
+		this.category = p_category;
+	}
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setNum(int p_isbn) {
+		this.isbn = p_isbn;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String p_title) {
+		this.title = p_title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String p_author) {
+		this.author = p_author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String p_publisher) {
+		this.publisher = p_publisher;
+	}
+	public Date getBookyear() {
+		return publication_year;
+	}
+
+	public void setBookyear(Date p_publication_year) {
+		this.publication_year = p_publication_year;
+	}
+	public String getCategory() {
+		return category;
+	}
+
+	public void set(String p_category) {
+		this.category = p_category;
+	}
 
 }
--- BOOK/src/DTO/rfid_card.java
+++ BOOK/src/DTO/rfid_card.java
@@ -1,5 +1,17 @@
 package DTO;
 
 public class rfid_card {
-
+	String card_key;
+	
+	public rfid_card(String p_card_key) {
+		this.card_key = p_card_key;
+	}
+	
+	public String getKey() {
+		return card_key;
+	}
+	
+	public void setKey(String p_card_key) {
+		this.card_key = p_card_key;
+	}
 }
