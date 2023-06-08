26bcfb616a19454c7a91ca3b77dc7d1798d5b735
TAEUNG-NAM <106743597+TAEUNG-NAM@users.noreply.github.com>
2023-06-09 01:31:13 +0900

    대여정보 기능 구현 (#25)
    * 로그인-JSP-DB 연동
    
    로그인-JSP-DB 연동
    
    * 학교 마크 수정
    
    흰색 배경을 투명으로 수정
    
    * 대여정보 수정중
    
    대여정보 jsp접속기능 수정중
    
    * 대여정보 수정중
    
    1. 대여정보에서 JSP(대여정보) 연동완료
    2. 추가로 리스트에 JSON값 추가해야함
    
    * 대여정보 기능 구현
    
    1. 대여정보 기능 구현
    2. 추후 최적화 필요

--- LibraryApp/app/src/main/java/com/example/libraryapp/Frag_Rentalinfo.java
+++ LibraryApp/app/src/main/java/com/example/libraryapp/Frag_Rentalinfo.java
@@ -1,17 +1,13 @@
 package com.example.libraryapp;
 
-import android.animation.ValueAnimator;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,59 +18,63 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.net.HttpURLConnection;
 
 public class Frag_Rentalinfo extends Fragment { //대여정보 프래그먼트
-
     ArrayList<rentalData> rentaldata;    //어레이형식
+    private Handler handler;
     ListView rentalList;
     String result;
     static String ID;   // LoginActivity로 부터 사용자ID 받아올 정적변수
-
     private static CustomAdapter customAdapter;
+    LoginActivity logAct;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-
+//        handler = new Handler(Looper.getMainLooper());  // 쓰레드 사용을 위한 핸들러 생성(getMainLooper() 메인쓰레드호출)
+        logAct = new LoginActivity();
         View view = inflater.inflate(R.layout.frag_rentalinfo, container, false); //레이아웃 inflate로 객체화
-
         rentaldata = new ArrayList<>();
-        LoginActivity logAct  = new LoginActivity();
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                logAct.id = ID;     // LoginActivity에서 받아온 사용자id를 다시 LoginActivity로 넘겨주기
-                result = logAct.connect(2  );   // 2일때 유저의 대여정보에 접속(JSP)
-            }
-        });
-        t2.start();
 
-        // 쓰레드 안에 넣는법 찾아야함
-//        try { 
-//            JSONArray jsonArray = new JSONArray(result);
-//            for(int i = 0; i<jsonArray.length(); i++){
-//                JSONObject jsonObject = jsonArray.getJSONObject(i);
-//                rentaldata.add(new rentalData(jsonObject.getString("책제목"), jsonObject.getString("대여일"), jsonObject.getString("반납일")));
-//            }
-//
-//        } catch (JSONException e) {
-//            throw new RuntimeException(e);
-//        }
-
-
-        rentaldata.add(new rentalData("니체의말", "김민철", "2020"));
-//        rentaldata.add(new rentalData("명품C++", "남태웅", "2024"));
-//        rentaldata.add(new rentalData("자바의 정석", "남궁성", "2030"));
-//        rentaldata.add(new rentalData("소프트웨어 공학", "장성진", "2050"));
-        rentalList = view.findViewById(R.id.rental_List);
-        customAdapter = new CustomAdapter(getContext(), rentaldata);
-        rentalList.setAdapter(customAdapter);
-
+        new BackgroundThread().start();    // 쓰레드 호출
+//        rentaldata.add(new rentalData("니체의말", "김민철", "2020"));
         return view;
     }
+
+    private class BackgroundThread extends Thread {
+        @Override
+        public void run() {
+            // 백그라운드 작업 수행
+
+            logAct.id = ID;
+            String result = logAct.connect(2);
+
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String bookName = jsonObject.getString("책제목");
+                    String startRental = jsonObject.getString("대여일");
+                    String endRental = jsonObject.getString("반납일");
+                    rentaldata.add(new rentalData(bookName, startRental, endRental));
+                }
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+            // UI 업데이트를 위해 메인 쓰레드에 메시지 전송
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // UI 업데이트 작업 수행
+                    rentalList = getView().findViewById(R.id.rental_List);
+                    customAdapter = new CustomAdapter(getContext(), rentaldata);
+                    rentalList.setAdapter(customAdapter);
+                }
+            });
+        }
+    }
+
 }
 
 class rentalData{
